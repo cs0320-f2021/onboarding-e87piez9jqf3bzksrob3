8867f76a3b34f75ad81a9a1171ce3e1d8e6047ad
e87piez9jqf3bzksrob3 <e87piez9jqf3bzksrob3@gmail.com>
2021-09-20 02:38:52 -0400

    style: removed todo comment

--- src/test/java/edu/brown/cs/student/main/MathBotTest.java
+++ src/test/java/edu/brown/cs/student/main/MathBotTest.java
@@ -27,8 +27,6 @@ public class MathBotTest {
     assertEquals(1, output, 0.01);
   }
 
-  // TODO: add more unit tests of your own
-
   @Test
   public void testIntOperations(){
     MathBot mathDude = new MathBot();
